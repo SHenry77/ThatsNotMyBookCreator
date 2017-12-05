1094f8e22b9837acfac95b98c22e71e9c2bf810b
SHenry77 <sarahhenry77@gmail.com>
2017-12-05 17:50:12 +0000

    refactored to remove unnecessary complication of page.

--- src/com/learning/thatsnotmybook/Book.java
+++ src/com/learning/thatsnotmybook/Book.java
@@ -2,33 +2,60 @@ package com.learning.thatsnotmybook;
 
 public class Book {
 
-    protected static String THATS_NOT_MY = "That's not my ";
-    private String titlularNoun;
-    private Page[] pages;
+    private static String IT_IS_TOO = "It's %s is too %s.";
+    private static String IT_IS_SO = "It's %s is so %s.";
+    private static String THATS_NOT_MY = "That's not my ";
+    private static String THATS_MY = "That's my ";
+    private String titularNoun;
 
-    public Book(String titlularNoun, Page[] pages){
-        this.titlularNoun = titlularNoun;
-        this.pages = pages;
+    private String[] nouns;
+    private String[] adjectives;
+
+    Book(String titularNoun, String[] nouns, String[] adjectives){
+        this.titularNoun = titularNoun;
+        this.nouns = nouns;
+        this.adjectives = adjectives;
     }
 
-    public Book(String titlularNoun, String[] nouns, String[] adjectives){
-        this.titlularNoun = titlularNoun;
-        pages = new Page[nouns.length];
-        for(int i=0; i<nouns.length; i++){
-            pages[i]= new Page(nouns[i], adjectives[i], i == nouns.length-1);
+    public String getTitularNoun() {
+        return titularNoun;
+    }
+
+    String getTitle(){
+        return THATS_NOT_MY + titularNoun;
+    }
+
+    public String[] getNouns(){
+        return nouns;
+    }
+
+    public String[] getAdjectives(){
+        return adjectives;
+    }
+
+    String getText(){
+        StringBuilder builder = new StringBuilder();
+        for(int page=0; page< nouns.length; page++){
+            builder.append(getPageText(page));
         }
+        return builder.toString();
     }
 
-    public String getTitlularNoun() {
-        return titlularNoun;
-    }
-
-    public String getTitle(){
-        return THATS_NOT_MY + titlularNoun;
-    }
-
-    public Page[] getPages(){
-        return pages;
+    String getPageText(int pageNum){
+        boolean finalPage = pageNum == nouns.length-1;
+        StringBuilder builder = new StringBuilder();
+        if(finalPage){
+            builder.append(THATS_MY );
+            builder.append(titularNoun);
+            builder.append(". ");
+            builder.append(String.format(IT_IS_SO, nouns[pageNum], adjectives[pageNum]));
+        } else {
+            builder.append(getTitle());
+            builder.append(". ");
+            builder.append(String.format(IT_IS_TOO, nouns[pageNum], adjectives[pageNum]));
+            builder.append(" ");
+        }
+        return builder.toString();
     }
 
 }
--- src/com/learning/thatsnotmybook/Main.java
+++ src/com/learning/thatsnotmybook/Main.java
@@ -4,22 +4,15 @@ public class Main {
     public static void main(String[] args){
         String[]nouns = new String[]{"cover", "page", "text", "story", "plot"};
         String[]adjectives = new String[]{"rough", "smooth", "short", "interesting", "predictable"};
-        Page[] pages = new Page[5];
-        for(int i=0; i<nouns.length; i++){
-            boolean finalPage = i == nouns.length-1;
-            pages[i] = new Page(nouns[i], adjectives[i], finalPage);
-        }
-        Book book = new Book("book", pages);
-        Book book2 = new Book( "Book", nouns, adjectives);
-        readBook(book);
+
+        Book book2 = new Book( "book", nouns, adjectives);
         readBook(book2);
+        book2.getPageText(2);
     }
 
-    public static void readBook(Book book){
+    private static void readBook(Book book){
         System.out.println(book.getTitle());
-        for(Page page: book.getPages()){
-            System.out.println(page.getText());
-        }
+        System.out.println(book.getText());
     }
 
 }
